63cb1dcb7333be31b321fa23aa5d36de97807289
udohette <udohette@gmail.com>
2019-10-04 03:49:27 +0100

    Subscription code class added

--- app/src/main/java/com/example/techflex_e_literacy/quiz/SubscriptionCodeActivity.java
+++ app/src/main/java/com/example/techflex_e_literacy/quiz/SubscriptionCodeActivity.java
@@ -1,8 +1,10 @@
 package com.example.techflex_e_literacy.quiz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 
 import com.example.techflex_e_literacy.R;
 import com.example.techflex_e_literacy.mainActivity.PasswordPopUp;
+import com.example.techflex_e_literacy.mainActivity.UserActivity;
 
 import java.security.SecureRandom;
 
@@ -34,29 +37,32 @@ public class SubscriptionCodeActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        final PasswordPopUp ssnPopUp = new PasswordPopUp(SubscriptionCodeActivity.this);
-        ssnPopUp.setCancelable(true);
-        ssnPopUp.setCanceledOnTouchOutside(false);
-        ssnPopUp.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
-        ssnPopUp.getWindow().setBackgroundDrawable(new ColorDrawable(Color.YELLOW));
-        ssnPopUp.show();
-        final TextView tvSubmit = ssnPopUp.findViewById(R.id.tv_submit);
-        final EditText etSSN = ssnPopUp.findViewById(R.id.et_ssn);
+        showPopUp();
+    }
 
-        tvSubmit.setOnClickListener(new View.OnClickListener() {
+    void showPopUp3() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SubscriptionCodeActivity.this);
+        builder.setIcon(R.drawable.noun1);
+        builder.setTitle("Invalid Activation Code");
+        builder.setMessage("Kindly retype code carefully\n contact support for assistance");
+        builder.setPositiveButton("retry", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!TextUtils.isEmpty(etSSN.getText().toString().trim())
-                        && TextUtils.equals(etSSN.getText().toString(), password)) {
-                    startActivity(new Intent(SubscriptionCodeActivity.this, QuizActivity.class));
-                } else {
-                    Toast.makeText(SubscriptionCodeActivity.this, "Invalid Activation Code", Toast.LENGTH_SHORT).show();
-                }
-                ssnPopUp.dismiss();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                showPopUp();
+
+            }
+
+        });
+        builder.setNegativeButton("quit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                startActivity(new Intent(SubscriptionCodeActivity.this, UserActivity.class));
             }
         });
-        Window window = ssnPopUp.getWindow();
-        window.setLayout(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT);
+        builder.show();
+        AlertDialog alertDialog = builder.show();
+        alertDialog.setCancelable(false);
+        alertDialog.setCanceledOnTouchOutside(false);
     }
 
     private String generateRandomKey(int len) {
@@ -69,5 +75,34 @@ public class SubscriptionCodeActivity extends AppCompatActivity {
 
         Log.e("Password", sb.toString());
         return sb.toString();
+    }
+
+    public void showPopUp() {
+        final PasswordPopUp ssnPopUp = new PasswordPopUp(SubscriptionCodeActivity.this);
+        ssnPopUp.setCancelable(true);
+        ssnPopUp.setCanceledOnTouchOutside(false);
+        ssnPopUp.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
+        //ssnPopUp.getWindow().setBackgroundDrawable(new ColorDrawable(Color.YELLOW));
+        ssnPopUp.show();
+        final TextView tvSubmit = ssnPopUp.findViewById(R.id.tv_submit);
+        final EditText etSSN = ssnPopUp.findViewById(R.id.et_ssn);
+
+        tvSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TextUtils.isEmpty(etSSN.getText().toString().trim())
+                        && TextUtils.equals(etSSN.getText().toString(), password)) {
+                    startActivity(new Intent(SubscriptionCodeActivity.this, QuizActivity.class));
+                } else {
+                    showPopUp3();
+                    //Toast.makeText(SubscriptionCodeActivity.this, "Invalid Activation Code", Toast.LENGTH_SHORT).show();
+                }
+                ssnPopUp.dismiss();
+
+            }
+        });
+        Window window = ssnPopUp.getWindow();
+        window.setLayout(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT);
+
     }
 }
