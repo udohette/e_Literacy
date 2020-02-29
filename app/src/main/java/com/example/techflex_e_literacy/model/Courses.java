927940180c3b86265dfd83b08c3bcdb0b0bd3a76
ankit <ankit.kishore@techolution.com>
2020-02-29 18:08:33 +0530

    done

---
+++ app/src/main/java/com/example/techflex_e_literacy/model/Courses.java
@@ -0,0 +1,22 @@
+package com.example.techflex_e_literacy.model;
+
+public class Courses {
+    String courseReg;
+    String endDate;
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCourseReg() {
+        return courseReg;
+    }
+
+    public void setCourseReg(String courseReg) {
+        this.courseReg = courseReg;
+    }
+}
